855662430eb539cb490497658a64cd2478934a86
Marek Jóźwiak <168720167+66HEX@users.noreply.github.com>
2025-09-15 22:12:09 +0200

    feat: implement border radius config via css

--- src/utils/tailwind.ts
+++ src/utils/tailwind.ts
@@ -9,14 +9,14 @@ export async function addDesignTokensToCss(
 	const V4_SNIPPET = `@import "tailwindcss";
 
 :root {
-	--color-background: oklch(0.97 0 0);
-	--color-background-muted: oklch(0.922 0 0);
+	--color-background: oklch(0.99 0 0);
+	--color-background-muted: oklch(0.952 0 0);
 	--color-background-elevated: oklch(0.87 0 0);
 	--color-foreground: oklch(0.205 0 0);
 	--color-foreground-muted: oklch(0.371 0 0);
 	--color-foreground-subtle: oklch(0.708 0 0);
 	--color-border: oklch(0.205 0 0);
-	--color-border-muted: oklch(0.922 0 0);
+	--color-border-muted: oklch(0.952 0 0);
 	--color-border-subtle: oklch(0.708 0 0);
 	--color-ring: oklch(0.205 0 0);
 	--color-ring-offset: oklch(0.97 0 0);
@@ -24,20 +24,21 @@ export async function addDesignTokensToCss(
 	--color-primary-foreground: oklch(0.97 0 0);
 	--color-primary-muted: oklch(0.371 0 0);
 	--color-overlay: oklch(0.97 0 0);
-	--color-gradient-primary-start: oklch(0.371 0 0);
+	--color-gradient-primary-start: oklch(0.391 0 0);
 	--color-gradient-primary-end: oklch(0.205 0 0);
 	--color-primary-white: oklch(0.97 0 0);
+	--radius-base: 0.125rem;
 }
 
 .dark {
 	--color-background: oklch(0.205 0 0);
-	--color-background-muted: oklch(0.269 0 0);
+	--color-background-muted: oklch(0.259 0 0);
 	--color-background-elevated: oklch(0.371 0 0);
 	--color-foreground: oklch(0.97 0 0);
 	--color-foreground-muted: oklch(0.87 0 0);
 	--color-foreground-subtle: oklch(0.556 0 0);
 	--color-border: oklch(0.97 0 0);
-	--color-border-muted: oklch(0.269 0 0);
+	--color-border-muted: oklch(0.259 0 0);
 	--color-border-subtle: oklch(0.371 0 0);
 	--color-ring: oklch(0.97 0 0);
 	--color-ring-offset: oklch(0.205 0 0);
@@ -45,13 +46,12 @@ export async function addDesignTokensToCss(
 	--color-primary-foreground: oklch(0.205 0 0);
 	--color-primary-muted: oklch(0.87 0 0);
 	--color-overlay: oklch(0.145 0 0);
-	--color-gradient-primary-start: oklch(0.371 0 0);
+	--color-gradient-primary-start: oklch(0.391 0 0);
 	--color-gradient-primary-end: oklch(0.2768 0 0);
-	--color-primary-white: oklch(0.922 0 0);
-}
+	--color-primary-white: oklch(0.952 0 0);
+  	--radius-base: 0.125rem;}
 
 @theme {
-	--font-sfpro: var(--font-sfpro);
 	--color-background: var(--background);
 	--color-background-muted: var(--background-muted);
 	--color-background-elevated: var(--background-elevated);
@@ -70,6 +70,16 @@ export async function addDesignTokensToCss(
 	--color-gradient-primary-end: var(--gradient-primary-end);
 	--color-overlay: var(--overlay);
 	--color-primary-white: var(--primary-white);
+	--radius-sm: calc(var(--radius) * 0.5);
+	--radius-xs: calc(var(--radius-base) * 1);
+	--radius-sm: calc(var(--radius-base) * 2);
+	--radius-md: calc(var(--radius-base) * 3);
+	--radius-lg: calc(var(--radius-base) * 4);
+	--radius-xl: calc(var(--radius-base) * 6);
+	--radius-2xl: calc(var(--radius-base) * 8);
+	--radius-3xl: calc(var(--radius-base) * 12);
+	--radius-4xl: calc(var(--radius-base) * 16);
+	--radius-full: 9999px;
 }`;
 
 	try {
